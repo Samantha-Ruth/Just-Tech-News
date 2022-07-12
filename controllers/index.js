1e988df1d9c5c10e80986ea64b8b1c8b05512fba
Samantha-Ruth <sam.haberman@gmail.com>
2022-07-12 15:40:22 -0600

    created front end javascript logic, as well as creating a session on the backend and a method to destroy that session

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,4 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
-
 module.exports = router;
